3d305e427993f2fd39f000db6014302aad3f11ce
shamishma <150496023+shamishma@users.noreply.github.com>
2023-11-11 07:59:07 +0530

    Create ch1_a.c

---
+++ chapter1/ch1_a.c
@@ -0,0 +1,16 @@
+/*
+Program
+
+*/
+
+
+#include <stdio.h>
+int main()
+{
+  float f,c;
+printf("Enter temp in f :");
+scanf("%f",&f);
+  c = (f-32)*5/9;
+printf("\nTemperature of two cities in c is                 : %f",c);
+ return 0;
+}
