665f0a95544b3e93b0079fe3938d6bb4ecc66670
shamishma <150496023+shamishma@users.noreply.github.com>
2023-11-22 21:15:44 +0530

    Update ch3_1.c

--- chapter_3/extra/ch3_1.c
+++ chapter_3/extra/ch3_1.c
@@ -1,1 +1,20 @@
-s
+#include<stdio.h>
+int main()
+{
+    float a,b,result;
+    printf("Enter the first number  : "); 
+    scanf("%f",&a);
+    printf("Entre the second number : ");
+    scanf("%f",&b);
+    result=a*b;
+    printf("The result is : %.2f\n",result);
+    if(result>100)
+    {
+        printf("The result is grater than hundred \n"); 
+    }
+    else
+    {
+        printf("The result is less than hundred\n");
+    }
+       return 0;
+}
