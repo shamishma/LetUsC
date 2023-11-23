c4f50d004705704e2bf1eb32f2d604958905de3b
shamishma <150496023+shamishma@users.noreply.github.com>
2023-11-23 20:53:07 +0530

    Update extra

---
+++ chapter_3/extra/ch3_11.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    float marks;
+    printf("Enter the marks : ");
+    scanf("%f",&marks);
+    if (marks>90)
+        printf("Grade A");
+    else
+        {
+            if(marks>80)
+            printf("Grade B");
+            else
+            {
+                if(marks>70)
+                printf("Grade C");
+                else
+                {
+                    if(marks>60)
+                    printf("Grade D");
+                    else
+                    {
+                        if(marks>50)
+                        printf("Grade E ");
+                        else
+                        {
+                            if(marks<40)
+                            printf("you are fail !");
+                        }
+                    }
+                }
+            }
+        }
+    
+    return 0;
+}
