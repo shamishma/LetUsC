1d91866b149462cfddcb6e685b2e8c75db36f73d
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-08 18:55:38 +0530

    Update extra

---
+++ chapter 5/extra/ch5_4.c
@@ -0,0 +1,17 @@
+/*
+Program
+
+*/
+
+
+#include <stdio.h>
+int main()
+{
+  float sal;
+printf("Enter the salary:");
+scanf("%f",&sal);
+((sal>=25000 && sal<=40000)?printf("Manager!"):(sal>15000 && sal<25000))? printf("Accountant!"):printf("clek!");
+ return 0;
+}
+
+
