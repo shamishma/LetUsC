db44eb286ecbfd4e333fb2ddc478895f5e98dc56
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-08 18:44:50 +0530

    Update exercise (2)

---
+++ chapter_4/exercise (2)/ch4_9.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float x,si,co,sum;
+    printf("Enter the angle : ");
+    scanf("%f",&x);
+    x = x * 3.14159265359/180;
+    si = sin(x);
+    co = cos(x);
+    sum = si*si + co*co;
+    if(sum  == 1)
+    {
+        printf("Yes this is equal to 1");
+    }
+    else
+    {
+        printf("No this is not equal to 1");
+    }
+    return 0;
+}
