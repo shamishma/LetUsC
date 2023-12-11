f7e358a5bcf665b97a577600d015e3482edbfc49
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-11 20:58:59 +0530

    Update exercise 2

---
+++ chapter 5/exercise 2/ch5_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num, oct=0, rem,i=0;
+    printf("Enter the number : ");
+    scanf("%d", &num);
+    while (num != 0)
+    {
+        rem = num % 8;
+        num = num / 8;
+        oct = oct + rem * pow(10,i);  
+        i++;
+    }
+    printf("Octal Value of entered number is %d",oct);
+    return 0;
+}
