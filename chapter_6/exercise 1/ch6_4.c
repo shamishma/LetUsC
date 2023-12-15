333435598c7ee6ad5f6ce2dc81719ec107973a4e
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-15 12:06:13 +0530

    Update exercise 1

---
+++ chapter_6/exercise 1/ch6_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i, sum = 0, n;
+    printf("Enter the term : ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i+=i)
+    {
+        sum+=i;   // sum = sum + i
+        // fact = fact * i;        fact*=i
+    }
+    printf("sum = %d", sum);
+    return 0;
+}
