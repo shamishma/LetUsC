08731fc2cdcff545dab56604cecb06263c7fd83e
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-15 12:04:27 +0530

    Update

---
+++ chapter_6/exercise 1/ch6_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int num, fact=1;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    for(i=num;i>=1;i--)
+    {
+        fact = fact * i;
+    }    
+    printf("Factorial of %d is %d",num,fact);
+    return 0;
+}
