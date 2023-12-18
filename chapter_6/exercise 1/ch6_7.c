e55f65a5fc0628dc681d6e83d62d79f40ab3600e
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-18 21:28:08 +0530

    Update exercise 1

---
+++ chapter_6/exercise 1/ch6_7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+ {
+    int num,i;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+for(i=2;i<=num-1;i++)
+{
+  if(num%i==0)
+  {
+    printf("Not a prime number!");
+    break;
+    }
+    }
+    if(i==num)
+    {
+      printf("Prime number!");
+      }
+    return 0;
+}
