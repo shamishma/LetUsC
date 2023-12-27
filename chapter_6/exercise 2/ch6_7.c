5f97bb78026ed306d581035f4cc1a8fc76cd8179
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-27 16:53:02 +0530

    Update exercise 2

---
+++ chapter_6/exercise 2/ch6_7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k;
+    int n =1;
+    for (i = 1; i <= 4; i++)
+    {
+        for (j = 3; j >= i; j--)
+        {
+            printf("  ");
+        }
+            
+
+        for (k = 1; k <= i; k++)
+        {
+            printf("%d   ",n);
+            n++;
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
