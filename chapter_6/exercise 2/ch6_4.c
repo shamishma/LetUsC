5134ed86cca56447b635cb41658a5c2d898f9ef0
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-27 16:50:38 +0530

    Update exercise 2

---
+++ chapter_6/exercise 2/ch6_4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,h;
+    for(l=1;l<=30;l++)
+    {
+        for(b=l;b<=30;b++)
+        {
+            for(h=1;h<=30;h++)
+            {
+                if(l*l + b*b == h*h)
+                {
+                    printf("Pythagorean triplets : %d*%d +  %d*%d = %d*%d\n",l,l,b,b,h,h);
+                }
+            }
+        }
+        
+    }
+    printf("\n");
+    return 0;
+}
