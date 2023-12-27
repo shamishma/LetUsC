23e3344178c156c19df27c76af9c3be32c102085
shamishma <150496023+shamishma@users.noreply.github.com>
2023-12-27 17:32:55 +0530

    Update pattern

---
+++ chapter_6/pattern/ch6_4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=4;i++)
+    {
+        for(j=2;j<=i;j++)
+        {
+            printf(" ");
+        }
+        for(k=4;k>=i;k--)
+       {
+         printf("* ");
+       }
+        printf("\n");
+    }
+for(i=1;i<=4;i++)
+    {
+        for(j=3;j>=i;j--)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=i;k++)
+       {
+         printf("* ");
+       }
+        printf("\n");
+    }  
+return 0;
+}
