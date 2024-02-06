4efb200c3102cf9aadaa96e1595b0eecca74a370
shamishma <karishmasingh0219@gmail.com>
2024-02-06 15:20:44 +0530

    update

--- chapter_5/extra/ch5_4.c
+++ chapter_5/extra/ch5_4.c
@@ -8,7 +8,7 @@ Program
 int main()
 {
   float sal;
-printf("Enter the salary:");
+printf("Enter the ry:");
 scanf("%f",&sal);
 ((sal>=25000 && sal<=40000)?printf("Manager!"):(sal>15000 && sal<25000))? printf("Accountant!"):printf("clerk!");
  return 0;
