412d52d19282dc6eca99b23cd3ce5d27f919ab9b
shamishma <karishmasingh0219@gmail.com>
2024-02-24 07:43:07 +0530

    update

--- exersice_2/6.c
+++ exersice_2/6.c
@@ -1,15 +1,15 @@
 /*Find the output*/
 #include <stdio.h>
-void function(int *);
+int function(int );
 int main()
 {
     int i = 35, *z;
     z = &i;
-    *z = function(&i);
+    *z = function(i);
     printf("%d\n", *z);
     return 0;
 }
-void function(int *m)
+int function(int m)
 {
-    return (*m + 2);
+    return (m + 2);
 }
