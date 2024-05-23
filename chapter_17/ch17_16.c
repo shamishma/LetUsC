45c8bb8693fbe3be02187580a69b57c3ecec446e
shamishma <karishmasingh0219@gmail.com>
2024-05-23 08:01:39 +0530

    update

--- chapter_17/ch17_16.c
+++ chapter_17/ch17_16.c
@@ -1,70 +1,81 @@
 /* Create a structure called library to hold accession number,title of the book,author name,price of
  the book,and flag indicating whether book is issued or not,write a menu-driven program that implements
  the working of a library.the menu option should be:
- 1.Add book information . 2.Display book information.*/
- #include <stdio.h>
-#include <stdbool.h> 
-struct Book 
+ 1.Add book information .
+ 2.Display book information.
+ 3.List all books of given author
+ 4.List the title of specified book
+ 5.List the count in the library
+ 6.List the book in the order of accession number*/
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX_BOOK 100
+struct record
 {
-    int accessionNumber;
-    char title[100];
-    char author[100];
+    char title[20];
+    char author[20];
     float price;
-    bool isIssued;
+    char accession[10];
+    char issue;
 };
-void addBook(struct Book library[], int *numBooks) 
+void input(struct record books[], int *n);
+void printAll(struct record books[], int n);
+int main()
 {
-    printf("Enter book information:\n");
-    printf("Accession Number: ");
-    scanf("%d", &library[*numBooks].accessionNumber);
-    printf("Title: ");
-    scanf("%s", library[*numBooks].title);
-    printf("Author: ");
-    scanf("%s", library[*numBooks].author);
-    printf("Price: ");
-    scanf("%f", &library[*numBooks].price);
-    printf("Is the book issued? (1 for yes, 0 for no): ");
-    scanf("%d", &library[*numBooks].isIssued);
-    
-    (*numBooks)++; 
-}
-void displayBooks(struct Book library[], int numBooks) 
-{
-    printf("Book Information:\n");
-    printf("Accession Number\tTitle\tAuthor\tPrice\tIssued\n");
-    for (int i = 0; i < numBooks; i++) {
-        printf("%d\t%s\t%s\t%.2f\t%s\n", library[i].accessionNumber, library[i].title, library[i].author, library[i].price, library[i].isIssued ? "Yes" : "No");
-    }
-}
-
-int main() 
-{
-    struct Book library[100]; 
-    int choice, numBooks = 0;
-
+    struct record books[MAX_BOOK];
+    int numBooks = 0, no;
     while (1)
-     {
-        printf("\nMenu:\n");
-        printf("1. Add book information\n");
-        printf("2. Display book information\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        
-        switch(choice) {
-            case 1:
-                addBook(library, &numBooks);
-                break;
-            case 2:
-                displayBooks(library, numBooks);
-                break;
-            case 3:
-                printf("Exiting program.\n");
-                return 0;
-            default:
-                printf("Invalid choice! Please enter a valid option.\n");
+    {
+        printf("Menu ==>\n");
+        printf("1.Add book information !\n");
+        printf("2.Display book information.!\n");
+        printf("3.List all books of given author.!\n");
+        printf("4.List the title of specified book.!\n");
+        printf("5.List the count in the library.!\n");
+        printf("6.List the book in the order of accession number.!\n");
+        printf("7.Exit!\n");
+        printf("==> ");
+        scanf("%d", &no);
+        switch (no)
+        {
+        case 1:
+            input(books, &numBooks);
+            break;
+        case 2:
+            printAll(books,numBooks);
+            break;
+
+        default:
+            printf("Please choose correct option!\n");
+            break;
         }
     }
-    
-    return 0;
+}
+void input(struct record books[], int *n)
+{
+    printf("Enter the record of book %d\n",*n + 1);
+    getchar();
+    printf("Enter the title of the book : ");
+    gets(books[*n].title);
+    printf("Enter the Author of the book : ");
+    gets(books[*n].author);
+    printf("Enter the Accession no. of the book : ");
+    gets(books[*n].accession);
+    printf("Enter the price of the book : ");
+    scanf("%f", &books[*n].price);
+    books[*n].issue = 'N';
+    *n = *n +1;
+}
+void printAll(struct record books[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("Book no. %d\n",i+1);
+        printf("Book name       : %s\n",books[i].title);
+        printf("Author name     : %s\n",books[i].author);
+        printf("Accession no.   : %s\n",books[i].accession);
+        printf("Price of book   : %.2f\n",books[i].price);
+        printf("Book issued     : %c\n",books[i].issue);
+    }
 }
